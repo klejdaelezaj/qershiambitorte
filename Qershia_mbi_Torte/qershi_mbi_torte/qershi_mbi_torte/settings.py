032b52ec6ddb8742217de563269162ef5d7927c0
klejdaelezaj <klejda_elezaj@yahoo.com>
2025-12-18 10:26:58 +0100

    Update settings.py

--- Qershia_mbi_Torte/qershi_mbi_torte/qershi_mbi_torte/settings.py
+++ Qershia_mbi_Torte/qershi_mbi_torte/qershi_mbi_torte/settings.py
@@ -156,12 +156,17 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 #Lidhja me email e order
+
+import os
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'qershiaambitortee@gmail.com'
-EMAIL_HOST_PASSWORD = 'cfbovrwxzxscwskv' 
+
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 # Media files (Images uploaded)
