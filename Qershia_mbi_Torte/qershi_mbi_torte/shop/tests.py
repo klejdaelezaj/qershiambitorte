14dbdd4e2bd96d3f83a64dae9774635ad1d7d142
klejdaelezaj <klejda_elezaj@yahoo.com>
2025-12-18 10:33:02 +0100

    Update tests.py

--- Qershia_mbi_Torte/qershi_mbi_torte/shop/tests.py
+++ Qershia_mbi_Torte/qershi_mbi_torte/shop/tests.py
@@ -1,3 +1,14 @@
+from django.core.mail import send_mail
+from django.conf import settings
 from django.test import TestCase
 
-# Create your tests here.
+class EmailTest(TestCase):
+
+    def test_send_email(self):
+        send_mail(
+            subject="TEST EMAIL NGA DJANGO",
+            message="Nëse ky email vjen, konfigurimi funksionon.",
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            recipient_list=[settings.EMAIL_HOST_USER],
+            fail_silently=False,
+        )
